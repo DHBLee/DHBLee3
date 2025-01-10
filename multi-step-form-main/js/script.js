82a6bf861160c242643f3c435ad6fca96757ebf8
Dong Hee Lee <dhblee0618@gmail.com>
2025-01-11 08:16:35 +1100

    updated

--- multi-step-form-main/js/script.js
+++ multi-step-form-main/js/script.js
@@ -7,10 +7,14 @@ const arcadeFees = document.getElementById('arcade-fees');
 const advancedFees = document.getElementById('advanced-fees');
 const proFees = document.getElementById('pro-fees');
 
+const monthlyFees = document.querySelectorAll('.step2-mothly');
+const yearlyFees = document.querySelectorAll('.step2-yearly'); 
+
 const root = document.documentElement;
 const purpleBlue = getComputedStyle(root).getPropertyValue('--clr-purpleBlue').trim();
 const marineBlue = getComputedStyle(root).getPropertyValue('--clr-marineBlue').trim();
 
+const planSwitcher = document.getElementById('label');
 const monthly = document.getElementById('monthly');
 const yearly = document.getElementById('yearly');
 
@@ -30,63 +34,64 @@ const confirmBtn = document.querySelectorAll('.confirmBtn');
 
 const fieldsets = document.querySelectorAll('.fieldset');
 
+let count = 1;
+const totalSteps = fieldsets.length;
+
 nextBtn.forEach(btn1 => btn1.addEventListener('click', nextStep));
 backBtn.forEach(btn2 => btn2.addEventListener('click', previousStep));
-let count = 1;
+planSwitcher.addEventListener('click', switchPlan);
 
 function nextStep() {
-    console.log('yehey');
-    if (count >= fieldsets.length) return;
-    count++;
-    updateStep();
+    if (count < totalSteps) {
+        count++;
+        updateStep();
+    }
 
 };
 
 function previousStep() {
-    if (count <= 1) return;
-    count--;
-    updateStep();
+    if (count > 1) {
+        count--;
+        updateStep();
+    }
 };
 
 function updateStep() {
-    fieldsets.forEach(field => { 
-        field.classList.remove('visible');
-        field.classList.add('hidden') 
+
+    fieldsets.forEach((field, index) => { 
+        field.classList.toggle('visible', index + 1 === count);
+        field.classList.toggle('hidden', index + 1 !== count); 
     });
-    const currentFieldset = document.getElementById(`fieldset-step${count}`);
-    if (currentFieldset) {
-        currentFieldset.classList.remove('hidden');
-        currentFieldset.classList.add('visible');
-    }
 
     stepsNumber.forEach((step, index) => {
-        step.classList.remove('active');
-
-        if (index + 1 === count) {
-            step.classList.add('active');
-        }
+        step.classList.toggle('active', index + 1 === count);
     });
 
-    backBtn.forEach(btn => {
-        btn.classList.toggle('invisible', count === 1);
-        btn.classList.toggle('visible', count !== 1);
-    })
-
-    if(count === 4) {
-        nextBtn.forEach(btn => {
-            btn.classList.remove('hidden');
-        })
-        confirmBtn.forEach(btn => {
-            btn.classList.add('hidden');
-        })
-    } else {
-        nextBtn.forEach(btn => {
-            btn.classList.add('hidden');
-        })
-        confirmBtn.forEach(btn => {
-            btn.classList.remove('hidden');
-        })
-    }
+    backBtn.forEach(btn => toggleButtonVisiblity(btn , count > 1));
+    nextBtn.forEach(btn => toggleButtonVisiblity(btn, count < totalSteps));
+    confirmBtn.forEach(btn => toggleButtonVisiblity(btn, count === totalSteps));
     
 }
 
+function toggleButtonVisiblity(button, isVisible) {
+    button.classList.toggle('hidden', !isVisible);
+    button.classList.toggle('visible', isVisible);
+}
+
+function switchPlan() {
+    console.log('dito napunta');
+    const isYearly = planSwitcher.style.transform === "translateX(20px)";
+    planSwitcher.style.transform = isYearly ? "translateX(0)" : "translateX(20px)";
+    isYearly ? monthlyPlan() : yearlyPlan();
+}   
+
+function yearlyPlan() {
+    console.log('napunta dito')
+    monthlyFees.forEach(monthly => monthly.classList.add('hidden'));
+    yearlyFees.forEach(yearly => yearly.classList.remove('hidden'));
+}
+
+function monthlyPlan() {
+    monthlyFees.forEach(monthly => monthly.classList.remove('hidden'));
+    yearlyFees.forEach(yearly => yearly.classList.add('hidden'));
+}
