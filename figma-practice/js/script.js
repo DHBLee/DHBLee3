1ef9f3dcf7af900a232dfb0eb91ab5d2f98f330e
Dong Hee Lee <dhblee0618@gmail.com>
2025-01-21 17:23:46 +1100

    updated

---
+++ figma-practice/js/script.js
@@ -0,0 +1,24 @@
+const menuBtn = document.getElementById('menuBtn');
+const loginBtn = document.getElementById('loginBtn');
+const logoutBtn = document.getElementById('logoutBtn');
+const profile = document.getElementById('profile');
+const introContainer = document.querySelector('.first__page')
+const settingsContainer = document.querySelector('.settings');
+const mainContainer = document.querySelector('.main');
+const headerContainer = document.querySelector('header')
+menuBtn.addEventListener('click', () => console.log('hello'))
+
+profile.addEventListener('click', () => {
+    mainContainer.style.display = mainContainer.style.display === 'none' ? "flex" : "none";   
+    settingsContainer.style.display = mainContainer.style.display === "none" ? "flex" : "none";
+})
+loginBtn.addEventListener('click', () => {
+    mainContainer.style.display = "flex";
+    headerContainer.style.display = "flex";
+    introContainer.style.display = "none"; 
+})
+logoutBtn.addEventListener('click', () => {
+    settingsContainer.style.display = "none";
+    introContainer.style.display = "flex"; 
+    headerContainer.style.display = "none";
+})
